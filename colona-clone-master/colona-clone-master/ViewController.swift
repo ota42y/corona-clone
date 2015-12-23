7563a4576a1f175b0391f40bf2a92ea4312bb24f
ota42y <ota42y@gmail.com>
2015-12-23 11:45:23 +0900

    show training point

--- colona-clone-master/colona-clone-master/ViewController.swift
+++ colona-clone-master/colona-clone-master/ViewController.swift
@@ -19,9 +19,18 @@ class ViewController: UIViewController {
     @IBOutlet weak var mainLabel: UILabel!
 
     var mode = Mode.Wait
+    var nowPoint: CGPoint!
+    var nowPointImageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        nowPoint = CGPoint.zero;
+
+        let img:UIImage?  = UIImage(named: "blue.png")
+        nowPointImageView = UIImageView(image: img)
+        view.addSubview(nowPointImageView)
+        nowPointImageView.hidden = true
 
         trainingButton.addTarget(self, action: "onClickTrainingButton:", forControlEvents: .TouchUpInside)
     }
@@ -31,16 +40,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
         if (mode == Mode.TrainingWait) {
             if let touch = touches.first {
-                let point = touch.locationInView(view)
-                NSLog("%f %f", point.x, point.y)
-
+                nowPoint = touch.locationInView(view)
+                showNowTrainingPoint()
             }
         }
     }
 
+    internal func showNowTrainingPoint() {
+        nowPointImageView.center = nowPoint
+        nowPointImageView.hidden = false
+    }
 
     internal func onClickTrainingButton(sender: UIButton){
         if (mode == Mode.Wait) {
